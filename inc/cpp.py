e7a96f65807b29e44ea7914d27801cc863423d37
eons <eons@eons.dev>
2022-01-13 22:22:59 -0800

    updates self to this

--- inc/cpp.py
+++ inc/cpp.py
@@ -9,129 +9,129 @@ from ebbs import Builder
 
 # Class name is what is used at cli, so we defy convention here in favor of ease-of-use.
 class cpp(Builder):
-    def __init__(self, name="C++ Builder"):
+    def __init__(this, name="C++ Builder"):
         super().__init__(name)
 
-        self.clearBuildPath = True
+        this.clearBuildPath = True
 
-        self.optionalKWArgs["cpp_version"] = 11
-        self.optionalKWArgs["cmake_version"] = "3.1.1"
-        self.optionalKWArgs["file_name"] = None
-        self.optionalKWArgs["libs_shared"] = None
+        this.optionalKWArgs["cpp_version"] = 11
+        this.optionalKWArgs["cmake_version"] = "3.1.1"
+        this.optionalKWArgs["file_name"] = None
+        this.optionalKWArgs["libs_shared"] = None
 
-        self.supportedProjectTypes.append("lib")
-        self.supportedProjectTypes.append("mod")
-        self.supportedProjectTypes.append("bin")
-        self.supportedProjectTypes.append("srv")
-        self.supportedProjectTypes.append("test")
+        this.supportedProjectTypes.append("lib")
+        this.supportedProjectTypes.append("mod")
+        this.supportedProjectTypes.append("bin")
+        this.supportedProjectTypes.append("srv")
+        this.supportedProjectTypes.append("test")
 
-        self.valid_cxx_extensions = [
+        this.valid_cxx_extensions = [
             ".cpp",
             ".h"
         ]
-        self.valid_lib_extensions = [
+        this.valid_lib_extensions = [
             ".a",
             ".so"
         ]
 
     # Required Builder method. See that class for details.
-    def DidBuildSucceed(self):
-        result = os.path.join(self.packagePath,self.file_name)
+    def DidBuildSucceed(this):
+        result = os.path.join(this.packagePath,this.file_name)
         logging.debug(f"Checking if build was successful; output should be {result}")
         return os.path.isfile(result)
 
     # Required Builder method. See that class for details.
-    def Build(self):
-        if (self.file_name is None):
-            self.file_name = self.projectName
+    def Build(this):
+        if (this.file_name is None):
+            this.file_name = this.projectName
 
-        self.packagePath = os.path.join(self.buildPath, "out")
-        mkpath(self.packagePath)
+        this.packagePath = os.path.join(this.buildPath, "out")
+        mkpath(this.packagePath)
 
-        logging.debug(f"Building in {self.buildPath}")
-        logging.debug(f"Packaging in {self.packagePath}")
+        logging.debug(f"Building in {this.buildPath}")
+        logging.debug(f"Packaging in {this.packagePath}")
 
-        self.GenCMake()
-        self.CMake(".")
-        self.Make()
+        this.GenCMake()
+        this.CMake(".")
+        this.Make()
 
         # include header files with libraries
-        if (self.projectType in ["lib"]):
-            copy_tree(self.incPath, self.packagePath)
+        if (this.projectType in ["lib"]):
+            copy_tree(this.incPath, this.packagePath)
 
-    def GetSourceFiles(self, directory, seperator=" "):
+    def GetSourceFiles(this, directory, seperator=" "):
         ret = ""
         for root, dirs, files in os.walk(directory):
             for f in files:
                 name, ext = os.path.splitext(f)
-                if (ext in self.valid_cxx_extensions):
+                if (ext in this.valid_cxx_extensions):
                     # logging.info(f"    {os.path.join(root, f)}")
                     ret += f"{os.path.join(root, f)}{seperator}"
         return ret[:-1]
 
-    def GetLibs(self, directory, seperator=" "):
+    def GetLibs(this, directory, seperator=" "):
         ret = ""
         for file in os.listdir(directory):
             if not os.path.isfile(os.path.join(directory, file)):
                 continue
             name, ext = os.path.splitext(file)
-            if (ext in self.valid_lib_extensions):
+            if (ext in this.valid_lib_extensions):
                 ret += (f"{name[3:]}{seperator}")
         return ret[:-1]
 
-    def GenCMake(self):
+    def GenCMake(this):
         # Write our cmake file
         cmakeFile = open("CMakeLists.txt", "w")
 
         cmakeFile.write(f'''
-cmake_minimum_required (VERSION {self.cmake_version})
-set (CMAKE_CXX_STANDARD {self.cpp_version})
-set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY {self.packagePath})
-set(CMAKE_LIBRARY_OUTPUT_DIRECTORY {self.packagePath})
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY {self.packagePath})
+cmake_minimum_required (VERSION {this.cmake_version})
+set (CMAKE_CXX_STANDARD {this.cpp_version})
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY {this.packagePath})
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY {this.packagePath})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY {this.packagePath})
 ''')
 
-        cmakeFile.write(f"project({self.file_name})\n")
+        cmakeFile.write(f"project({this.file_name})\n")
 
-        if (self.incPath is not None):
-            cmakeFile.write(f"include_directories({self.incPath})\n")
+        if (this.incPath is not None):
+            cmakeFile.write(f"include_directories({this.incPath})\n")
 
-        if (self.projectType in ["bin", "test", "srv"]):
+        if (this.projectType in ["bin", "test", "srv"]):
             logging.info("Addind binary specific code")
 
-            cmakeFile.write(f"add_executable({self.file_name} {self.GetSourceFiles(self.srcPath)})\n")
+            cmakeFile.write(f"add_executable({this.file_name} {this.GetSourceFiles(this.srcPath)})\n")
 
-        if (self.projectType in ["lib", "mod"]):
+        if (this.projectType in ["lib", "mod"]):
             logging.info("Adding library specific code")
 
             # #TODO: support windows install targets
             installSrcPath = f"/usr/local/lib"
-            installIncPath = f"/usr/local/include/{self.file_name}"
+            installIncPath = f"/usr/local/include/{this.file_name}"
 
-            cmakeFile.write(f"add_library ({self.file_name} SHARED {self.GetSourceFiles(self.srcPath)})\n")
+            cmakeFile.write(f"add_library ({this.file_name} SHARED {this.GetSourceFiles(this.srcPath)})\n")
             cmakeFile.write(
-                f"set_target_properties({self.file_name} PROPERTIES PUBLIC_HEADER \"{self.GetSourceFiles(self.incPath, ';')}\")\n")
+                f"set_target_properties({this.file_name} PROPERTIES PUBLIC_HEADER \"{this.GetSourceFiles(this.incPath, ';')}\")\n")
             cmakeFile.write(
-                f"INSTALL(TARGETS {self.file_name} LIBRARY DESTINATION {installSrcPath} PUBLIC_HEADER DESTINATION {installIncPath})\n")
+                f"INSTALL(TARGETS {this.file_name} LIBRARY DESTINATION {installSrcPath} PUBLIC_HEADER DESTINATION {installIncPath})\n")
 
         cmakeFile.write(f'''
 set(THREADS_PREFER_PTHREAD_FLAG ON)
 find_package(Threads REQUIRED)
-target_link_libraries({self.file_name} Threads::Threads)
+target_link_libraries({this.file_name} Threads::Threads)
 ''')
-        if (self.libPath is not None):
-            cmakeFile.write(f"include_directories({self.libPath})\n")
-            cmakeFile.write(f"target_link_directories({self.file_name} PUBLIC {self.libPath}))\n")
-            cmakeFile.write(f"target_link_libraries({self.file_name} {self.GetLibs(self.libPath)})\n")
+        if (this.libPath is not None):
+            cmakeFile.write(f"include_directories({this.libPath})\n")
+            cmakeFile.write(f"target_link_directories({this.file_name} PUBLIC {this.libPath}))\n")
+            cmakeFile.write(f"target_link_libraries({this.file_name} {this.GetLibs(this.libPath)})\n")
 
-        if (self.libs_shared is not None):
-            cmakeFile.write(f"target_link_libraries({self.file_name} {' '.join(self.libs_shared)})\n")
+        if (this.libs_shared is not None):
+            cmakeFile.write(f"target_link_libraries({this.file_name} {' '.join(this.libs_shared)})\n")
 
         cmakeFile.close()
 
-    def CMake(self, path):
-        self.RunCommand(f"cmake {path}")
+    def CMake(this, path):
+        this.RunCommand(f"cmake {path}")
 
-    def Make(self):
-        self.RunCommand("make")
+    def Make(this):
+        this.RunCommand("make")
 

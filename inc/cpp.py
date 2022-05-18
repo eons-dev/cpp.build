d261c7103abf4875727ccbf65fb2ccd378cb8958
eons <eons@eons.dev>
2022-05-17 17:10:16 -0700

    Adds support for compiler definitions, toolchains, installer config, and more.

--- inc/cpp.py
+++ inc/cpp.py
@@ -15,10 +15,16 @@ class cpp(Builder):
         this.clearBuildPath = True
 
         this.optionalKWArgs["cpp_version"] = 11
-        this.optionalKWArgs["cmake_version"] = "3.1.1"
+        this.optionalKWArgs["cmake_version"] = "3.12.0"
         this.optionalKWArgs["file_name"] = None
-        this.optionalKWArgs["libs_shared"] = None
+        this.optionalKWArgs["dep_lib"] = None
         this.optionalKWArgs["output_dir"] = "out"
+        this.optionalKWArgs["toolchain"] = None
+        this.optionalKWArgs["toolchain_dir"] = "tool"
+        this.optionalKWArgs["define"] = None
+        this.optionalKWArgs["install_bin_to"] = "/usr/local/bin"
+        this.optionalKWArgs["install_inc_to"] = "/usr/local/include"
+        this.optionalKWArgs["install_lib_to"] = "/usr/local/lib"
 
         this.supportedProjectTypes.append("lib")
         this.supportedProjectTypes.append("mod")
@@ -27,8 +33,10 @@ class cpp(Builder):
         this.supportedProjectTypes.append("test")
 
         this.valid_cxx_extensions = [
+            ".c",
             ".cpp",
-            ".h"
+            ".h",
+            ".hpp"
         ]
         this.valid_lib_extensions = [
             ".a",
@@ -37,28 +45,40 @@ class cpp(Builder):
 
     # Required Builder method. See that class for details.
     def DidBuildSucceed(this):
-        result = os.path.join(this.packagePath,this.file_name)
-        logging.debug(f"Checking if build was successful; output should be {result}")
-        return os.path.isfile(result)
+        result = this.packagePath
+        logging.debug(f"Checking if build was successful; output should be in {result}")
+        return bool(os.listdir(result))
 
     # Required Builder method. See that class for details.
     def Build(this):
         if (this.file_name is None):
             this.file_name = this.projectName
 
+        this.projectIsLib = False
+        if (this.projectType in ["lib", "mod"]):
+            this.projectIsLib = True
+
         this.packagePath = os.path.join(this.buildPath, this.output_dir)
         mkpath(this.packagePath)
+
+        this.toolPath = os.path.join(this.buildPath, this.toolchain_dir)
 
         logging.debug(f"Building in {this.buildPath}")
         logging.debug(f"Packaging in {this.packagePath}")
 
         this.GenCMake()
+        
+        if (this.projectIsLib):
+            this.GenSingleHeader()
+        
         this.CMake(".")
         this.Make()
 
         # include header files with libraries
-        if (this.projectType in ["lib"]):
+        if (this.projectIsLib):
             copy_tree(this.incPath, this.packagePath)
+
+        # this.GenInstall()
 
     def GetSourceFiles(this, directory, seperator=" "):
         ret = ""
@@ -73,7 +93,7 @@ class cpp(Builder):
     def GetLibs(this, directory, seperator=" "):
         ret = ""
         for file in os.listdir(directory):
-            if not os.path.isfile(os.path.join(directory, file)):
+            if (not os.path.isfile(os.path.join(directory, file))):
                 continue
             name, ext = os.path.splitext(file)
             if (ext in this.valid_lib_extensions):
@@ -86,35 +106,50 @@ class cpp(Builder):
 
         cmakeFile.write(f'''
 cmake_minimum_required (VERSION {this.cmake_version})
-set (CMAKE_CXX_STANDARD {this.cpp_version})
+set(CMAKE_CXX_STANDARD {this.cpp_version})
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY {this.packagePath})
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY {this.packagePath})
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY {this.packagePath})
 ''')
 
+        if (this.toolchain is not None):
+
+            # Make sure we have the toolchain.
+            if (not os.path.isdir(this.toolPath)):
+                toolchainSourcePath = os.path.join(this.executor.args.repo_store,f"toolchain_{this.toolchain}")
+                if (not os.path.isdir(toolchainSourcePath)):
+                    this.executor.DownloadPackage(f"toolchain_{this.toolchain}", registerClasses=False, createSubDirectory=True)
+                copy_tree(toolchainSourcePath, this.toolPath)
+
+            toolchainCmakeFile = f"{os.path.join(this.toolPath,this.toolchain)}.cmake"
+            if (not os.path.isfile(toolchainCmakeFile)):
+                raise OtherBuildError(f"Could not find cmake file: {toolchainCmakeFile}")
+            toolchainBinPath = os.path.join(this.toolPath, "bin")
+            cmakeFile.write(f"set(CROSS_TARGET_TOOLCHAIN_PATH {toolchainBinPath})\n")
+            cmakeFile.write(f"set(CMAKE_TOOLCHAIN_FILE {toolchainCmakeFile})\n")
+
         cmakeFile.write(f"project({this.file_name})\n")
 
         if (this.incPath is not None):
             cmakeFile.write(f"include_directories({this.incPath})\n")
 
-        if (this.projectType in ["bin", "test", "srv"]):
-            logging.info("Addind binary specific code")
-
+        if (this.projectIsLib):
+            logging.info("Adding library specific code")
+            cmakeFile.write(f"add_library ({this.file_name} SHARED {this.GetSourceFiles(this.srcPath)})\n")
+        else:
+            logging.info("Adding binary specific code")
             cmakeFile.write(f"add_executable({this.file_name} {this.GetSourceFiles(this.srcPath)})\n")
 
-        if (this.projectType in ["lib", "mod"]):
-            logging.info("Adding library specific code")
-
-            # #TODO: support windows install targets
-            installSrcPath = f"/usr/local/lib"
-            installIncPath = f"/usr/local/include/{this.file_name}"
-
-            cmakeFile.write(f"add_library ({this.file_name} SHARED {this.GetSourceFiles(this.srcPath)})\n")
-            cmakeFile.write(
-                f"set_target_properties({this.file_name} PROPERTIES PUBLIC_HEADER \"{this.GetSourceFiles(this.incPath, ';')}\")\n")
-            cmakeFile.write(
-                f"INSTALL(TARGETS {this.file_name} LIBRARY DESTINATION {installSrcPath} PUBLIC_HEADER DESTINATION {installIncPath})\n")
+        if (this.define is not None):
+            cmakeFile.write(f"add_compile_definitions(")
+            for key, value in this.define.items():
+                if (not value):
+                    cmakeFile.write(f"{key}")
+                else:
+                    cmakeFile.write(f"{key}={value}")
+                cmakeFile.write(" ")    
+            cmakeFile.write(")\n")
 
         cmakeFile.write(f'''
 set(THREADS_PREFER_PTHREAD_FLAG ON)
@@ -126,15 +161,114 @@ target_link_libraries({this.file_name} Threads::Threads)
             cmakeFile.write(f"target_link_directories({this.file_name} PUBLIC {this.libPath}))\n")
             cmakeFile.write(f"target_link_libraries({this.file_name} {this.GetLibs(this.libPath)})\n")
 
-        if (this.libs_shared is not None):
-            cmakeFile.write(f"target_link_libraries({this.file_name} {' '.join(this.libs_shared)})\n")
+        if (this.dep_lib is not None):
+            cmakeFile.write(f"target_link_libraries({this.file_name} {' '.join(this.dep_lib)})\n")
             cmakeFile.write('set(CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_CXX_COMPILER>  <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> -Wl,--start-group -Wl,--whole-archive <LINK_LIBRARIES> -Wl,--no-whole-archive -Wl,--end-group")') #per https://stackoverflow.com/questions/53071878/using-whole-archive-linker-option-with-cmake-and-libraries-with-other-library
 
         cmakeFile.close()
 
+
+    def GenSingleHeader(this):
+        includes = this.GetSourceFiles(this.incPath).split(' ')
+        includes = [os.path.relpath(path, this.incPath) for path in includes]
+
+        singleHeaderFile = open(os.path.join(this.packagePath,f"{this.projectName}.h"), "w+")
+        singleHeaderFile.write('#pragma once\n')
+        for i in includes:
+            singleHeaderFile.write(f"#include <{i}>\n")
+
+        singleHeaderFile.close()
+
+
+        # Create install.json, which will be used by emi to install what we've built.
+        # TODO: why not just use jsonpickle?
+    def GenInstall(this):
+        files = []
+        dirs = []
+        for thing in os.listdir(this.packagePath):
+            if (os.path.isdir(thing)):
+                dirs.append(thing)
+            else:
+                files.append(thing)
+
+        installFile = open(os.path.join(this.packagePath,"install.json"), "w+")
+        installFile.write('{\n')
+
+        if (this.dep_lib is not None):
+            installFile.write('    dep: [\n')
+            for i, d in enumerate(this.dep_lib):
+                installFile.write(f'        {d}')
+                if (i != len(files)-1):
+                    installFile.write(',\n')
+                else:
+                    installFile.write('\n')
+            installFile.write('    ],\n')
+
+        if (not this.projectIsLib):
+            installFile.write('    bin: [\n')
+
+            for i, f in enumerate(files):
+                installFile.write(f'        {f}')
+                if (i != len(files)-1):
+                    installFile.write(',\n')
+                else:
+                    installFile.write('\n')
+
+            for i, d in enumerate(dirs):
+                installFile.write(f'        {d}')
+                if (i != len(files)-1):
+                    installFile.write(',\n')
+                else:
+                    installFile.write('\n')
+
+            installFile.write('    ]\n')
+
+        if (this.projectIsLib):
+
+            #FIXME: THIS NEEDS WORK.
+
+            # Separate library files from include files.
+            # NOTE: this only parses files in the root of packagePath, not recursively.
+
+            includes = []
+            libraries = []
+
+            for f in files:
+                name, ext = os.path.splitext(f)
+                if (ext in this.valid_lib_extensions):
+                    libraries.append(f)
+                else:
+                    includes.append(f)
+
+            installFile.write('{\n')
+            installFile.write('    lib: [\n')
+
+            for i, f in enumerate(libraries):
+                installFile.write(f'        {f}')
+                if (i != len(files)-1):
+                    installFile.write(',\n')
+                else:
+                    installFile.write('\n')
+
+            installFile.write('    ],\n')
+            installFile.write('    inc: [\n')
+
+            for i, thing in enumerate(dirs + includes):
+                installFile.write(f'        {thing}')
+                if (i != len(files)-1):
+                    installFile.write(',\n')
+                else:
+                    installFile.write('\n')
+
+            installFile.write('    ]\n')
+
+        installFile.write('}\n')
+        installFile.close()
+
+
     def CMake(this, path):
         this.RunCommand(f"cmake {path}")
 
+
     def Make(this):
         this.RunCommand("make")
-
